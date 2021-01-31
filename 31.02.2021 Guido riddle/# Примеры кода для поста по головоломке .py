a95abef5d9a4a2a2114e5e378507da97850d61b2
gex <psiondnd@yandex.ru>
2021-01-31 14:00:32 +0500

    added material for post 31.01.2021

---
+++ 31.02.2021 Guido riddle/# Примеры кода для поста по головоломке .py
@@ -0,0 +1,62 @@
+# Примеры кода для поста по головоломке от Гвидо  ван Россума
+# https://zen.yandex.ru/media/id/5b7ae22633ef9b00a8cc79f3/python-golovolomka-6014e303d3c91450c6c0fd33
+
+# Пример с классом
+
+x = 0
+y = 0
+
+def f():
+    x = 1
+    y = 1
+    class C:
+        print(x, y)
+        x = 2
+
+f()
+
+# Пример с функцией
+
+x = 0
+y = 0
+
+def f():
+    x = 1
+    y = 1
+    def f2():
+        print(x, y)
+        x = 2
+    f2()
+
+f()
+
+# дизассемблирование сниппета с функцией
+
+import dis
+dis.dis("""
+x = 0
+y = 0
+def f():
+    x = 1
+    y = 1
+    def f2():
+        print(x, y)
+        x = 2
+    f2()
+f()
+""")
+
+# дизассемблирование сниппета с классом
+
+import dis 
+dis.dis(""" 
+x = 0 
+y = 0
+def f():
+     x = 1
+     y = 1
+     class C:
+         print(x, y)
+         x = 2
+f()
+""")
